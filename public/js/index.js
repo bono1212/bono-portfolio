a98ee843c89dea506b086af8741c63031d0e1a16
booldook <booldook@gmail.com>
2020-11-04 16:55:43 +0900

    booldook

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,6 @@
-$(".header-top .top-right").on("click", function(){
+/*$(".header-top .top-right").on("click", function(){
 	$(".menu-wrap").slideDown();
-});
+});*/
 $(".top-right .bar-box").on("click", function(){
-	$(".menu-wrap").slideUp();
+	$(".menu-wrap").stop().slideToggle();
 });
