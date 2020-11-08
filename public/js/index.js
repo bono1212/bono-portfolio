be5fde7bccfe6b43dff21e36ae6ab6774587afd7
bono1212 <uyanga1212g@gmail.com>
2020-11-09 00:39:38 +0900

    dfd

--- public/js/index.js
+++ public/js/index.js
@@ -2,3 +2,26 @@
 $(".top-right .bar-box").on("click", function(){
 	$(".menu-wrap").stop().slideToggle();
 });
+
+$(".scroll-wrap .bar2").on("click", function(){
+	$(".scroll-wrap .bar2").stop().css("width", "150%");
+	$(".scroll-wrap .bar1").stop().css("width", "100%");
+	$(".scroll-wrap .bar3").stop().css("width", "100%");
+	$(".scroll-wrap .bar4").stop().css("width", "100%");
+})
+$(".scroll-wrap .bar3").on("click", function(){
+	$(".scroll-wrap .bar3").stop().css("width", "150%");
+	$(".scroll-wrap .bar2").stop().css("width", "100%");
+	$(".scroll-wrap .bar1").stop().css("width", "100%");
+	$(".scroll-wrap .bar4").stop().css("width", "100%");
+})
+$(".scroll-wrap .bar4").on("click", function(){
+	$(".scroll-wrap .bar4").stop().css("width", "150%");
+	$(".scroll-wrap .bar3").stop().css("width", "100%");
+})
+$(".scroll-wrap .bar1").on("click", function(){
+	$(".scroll-wrap .bar1").stop().css("width", "150%");
+	$(".scroll-wrap .bar4").stop().css("width", "100%");
+	$(".scroll-wrap .bar2").stop().css("width", "100%");
+	$(".scroll-wrap .bar3").stop().css("width", "100%");
+})
