bf315a5234d3a5e023066a38390ac85bd65051e0
bono1212 <uyanga1212g@gmail.com>
2020-11-08 14:47:54 +0900

    ddd

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,4 @@
-/*$(".header-top .top-right").on("click", function(){
-	$(".menu-wrap").slideDown();
-});*/
+
 $(".top-right .bar-box").on("click", function(){
 	$(".menu-wrap").stop().slideToggle();
 });
