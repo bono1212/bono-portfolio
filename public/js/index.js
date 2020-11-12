2689f675e29eeea9871c7e07a56ccdd60d5bff68
bono1212 <uyanga1212g@gmail.com>
2020-11-12 19:26:22 +0900

    gg

--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,36 @@
 
+/* 	$(".mouse").mouseover(function() {
+		$(this).css("display", "block");
+	});
+	$("html, body").mousemove(onMove).hover(onHover, onLeave);
+
+	function onHover() {
+		$(".mouse").css("display", "block");
+	}
+
+	function onLeave() {
+		$(".mouse").css("display", "none");
+	}
+
+	function onMove(e) {
+		console.log(e)
+		var _x = e.pageX;
+		var _y = e.pageY;
+		$(".mouse").css({"left": _x+"px", "top": _y+"px"});
+	} */
+
+
+
+
+
 $(".top-right .bar-box").on("click", function(){
 	$(".menu-wrap").stop().slideToggle();
 });
 
+
+
+
+/* 
 $(".scroll-wrap .bar2").on("click", function(){
 	$(".scroll-wrap .bar2").stop().css("width", "150%");
 	$(".scroll-wrap .bar1").stop().css("width", "100%");
@@ -24,7 +52,7 @@ $(".scroll-wrap .bar1").on("click", function(){
 	$(".scroll-wrap .bar4").stop().css("width", "100%");
 	$(".scroll-wrap .bar2").stop().css("width", "100%");
 	$(".scroll-wrap .bar3").stop().css("width", "100%");
-})
+}) */
 
 /* $(".menus .h").on("click", function(){
 
