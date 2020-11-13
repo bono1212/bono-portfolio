774314492dce5c756c53ed90a015d4ac0987cfe6
bono1212 <uyanga1212g@gmail.com>
2020-11-13 17:20:40 +0900

    dddd

--- public/js/index.js
+++ public/js/index.js
@@ -20,12 +20,25 @@
 	} */
 
 
+/* *******scroll******** */
+
+$(window).on("scroll", onScroll);
+function onScroll() {
+	var sctop = $(this).scrollTop();
+	var page0 = $(".page-0").offset().top - $(this).height();
+	if(sctop > page0) {
+		$(".page-0").scrollTop();
+	}
+}
+
+
 
 
 /* *******menu page - show******* */
 $(".top-right .bar-box").on("click", function(){
 	$(".menu-wrap").stop().slideToggle();
 });
+
 
 /* *******show me********* */
 $(".page-1 .about-wrap .show-me").on("click", function(){
@@ -43,44 +56,6 @@ $(".page-2 .work-wrap .skills-bottom .bg").on("click", function(){
 })
 
 
-/* *******menu page에서 넘어가기******** */
 
 
 
-
-
-
-
-$(window).scrollTop()
-
-
-
-
-
-/* 
-$(".scroll-wrap .bar2").on("click", function(){
-	$(".scroll-wrap .bar2").stop().css("width", "150%");
-	$(".scroll-wrap .bar1").stop().css("width", "100%");
-	$(".scroll-wrap .bar3").stop().css("width", "100%");
-	$(".scroll-wrap .bar4").stop().css("width", "100%");
-})
-$(".scroll-wrap .bar3").on("click", function(){
-	$(".scroll-wrap .bar3").stop().css("width", "150%");
-	$(".scroll-wrap .bar2").stop().css("width", "100%");
-	$(".scroll-wrap .bar1").stop().css("width", "100%");
-	$(".scroll-wrap .bar4").stop().css("width", "100%");
-})
-$(".scroll-wrap .bar4").on("click", function(){
-	$(".scroll-wrap .bar4").stop().css("width", "150%");
-	$(".scroll-wrap .bar3").stop().css("width", "100%");
-})
-$(".scroll-wrap .bar1").on("click", function(){
-	$(".scroll-wrap .bar1").stop().css("width", "150%");
-	$(".scroll-wrap .bar4").stop().css("width", "100%");
-	$(".scroll-wrap .bar2").stop().css("width", "100%");
-	$(".scroll-wrap .bar3").stop().css("width", "100%");
-}) */
-
-/* $(".menus .h").on("click", function(){
-
-}) */
