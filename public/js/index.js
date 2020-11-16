dd95f9b94ef76bb31d19a2a84e018e663887e93b
bono1212 <uyanga1212g@gmail.com>
2020-11-16 17:50:34 +0900

    ccc

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,24 @@
+var now = 0;
+new Wheel (document, function(direction, e){
+	e.preventDefault();
+	var target = e.path.filter(function(v){
+	return $(v).hasClass('page');
+	});
+	var present = $(target[0]).index();
+	if(direction == 'up' && now > 0 ) {now--;} 
+	if(direction == 'down' && now < $(".page").length - 1) {now++;
+	}
+	pageAni();
+
+	function pageAni (){
+		$("html, body").stop().animate({"scrollTop": $(".page").eq(now).offset().top+"px"}, 800);
+	}
+});
+
+
+
+
+
 
 /* 	$(".mouse").mouseover(function() {
 		$(this).css("display", "block");
