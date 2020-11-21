0cc618e7db93f434990d7443f3d669fd94ee91e8
bono1212 <uyanga1212g@gmail.com>
2020-11-22 04:41:10 +0900

    vvv

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,19 @@
 $(".menu-bar").on("click", function () {
 	$(".titles").stop().slideToggle({direction: "left"}, 500);
 });
+
+
+
+
+
+
+
+
+
+// 이메일 발송
+emailjs.init("user_8iMQkOkWji9BqufHXmUZA");
+function mailSend(f) {
+	f.contact_number.value = Math.random() * 100000 | 0;
+	emailjs.sendForm('service_ggqs3fu', 'template_gfhuwtw', f);
+	return false;
+}
