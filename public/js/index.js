308f0c378c98390ed9c8458bf85589cfc0dcaad3
bono1212 <uyanga1212g@gmail.com>
2020-11-22 18:47:01 +0900

    ggg

--- public/js/index.js
+++ public/js/index.js
@@ -1,12 +1,57 @@
-/* $(".menu-bar").on("click", function () {
-	$(".titles").stop().slideToggle({direction: "left"}, 500);
-}); */
-
 $(".menu-bar").on("click", function () {
 	$(".titles").stop().animate({width: "toggle"});
 });
 
 
+$(function(){
+	var $menu = $(".titles .contents .menu"),
+			$contents = $(".page");
+			
+			$menu.click(function(){
+				var idx = $(this).index();
+				var section = $contents.eq(idx);
+				var sectionDis = section.offset().top;
+				// console.log(sectionDis);
+				$(".titles").stop().hide();
+				$('html, body').stop().animate({scrollTop: sectionDis});
+			});
+});
+
+
+
+
+/* back to top */
+var bt = document.getElementById('back-top'),
+		docElem = document.documentElement, offset, scrollPos, docHeight;
+
+		docHeight = Math.max(docElem.offsetHeight, docElem.scrollHeight);
+		if(docHeight == 0) {
+			offset = docHeight / 4;
+		}
+
+		window.addEventListener('scroll', function(){
+			scrollPos = docElem.scrollTop;
+			if(scrollPos > offset) {
+				bt.className = "visible";
+			} else {
+				bt.className = '';
+			}
+		});
+
+		bt.addEventListener('click', function(ev){
+			ev.preventDefault();
+			// docElem.scrollTop = 0;
+			scrollToTop();
+		}); 
+		function scrollToTop () {
+			var scrollInterval = setInterval(function(){
+				if(scrollPos !=0 ) {
+						window.scrollBy(0, -55);
+				} else {
+						clearInterval(scrollInterval);
+				}
+			}, 20);
+		}
 
 
 
